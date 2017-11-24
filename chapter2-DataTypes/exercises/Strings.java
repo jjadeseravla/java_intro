86a97ab21fa1831176bca06d514844d853d00dcd
jjade seravla <jadealvares@hotmail.co.uk>
2017-11-24 16:05:49 +0000

    jade and olly went to the park is working!

--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -5,7 +5,7 @@ String str1 = "Jade";
     // declare the second name
 String str2 = "Olly";
     // create your message
-String output = String.format("%s and %d went to the park.", str1, str2);
+String output = String.format("%s and %s went to the park.", str1, str2);
     // print your message!
 System.out.println(output);
   }

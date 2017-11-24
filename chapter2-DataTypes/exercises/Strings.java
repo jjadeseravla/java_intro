63422b7aca3606d0a10c081f4835e536f7f3ff60
jjade seravla <jadealvares@hotmail.co.uk>
2017-11-24 14:59:28 +0000

    struggling through chap 2

--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,12 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+String str1 = "Jade";
     // declare the second name
-
+String str2 = "Olly";
     // create your message
-
+String output = String.format("%s and %d went to the park.", str1, str2);
     // print your message!
-
+System.out.println(output);
   }
 }

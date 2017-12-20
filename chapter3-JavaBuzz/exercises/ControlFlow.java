e62c5f7c58f3ba47083ca396939a205e53e9d4b8
jjade seravla <jadealvares@hotmail.co.uk>
2017-12-20 11:59:16 +0000

    method checks whether number is odd or even

--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -2,13 +2,10 @@
 
 // can you remember how to set up your main function?
 
-// find a way to read in user input
 
-// write a method that will check if a number is odd or even
 // (assume user only ever enters integers)
 
 // print the answer to the console
-import java.util.Scanner;
 //
 // public class ControlFlow {
 //   public static void main(String[] args) {
@@ -18,11 +15,19 @@ import java.util.Scanner;
 //   }
 // }
 
+
+// find a way to read in user input
+import java.util.Scanner;
+
  public class ControlFlow {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);  // Reading from System.in
     System.out.println("Enter a number: ");
     int n = reader.nextInt(); // Scans the next token of the input as an int.
+    // write a method that will check if a number is odd or even
+    boolean isEven = (n % 2 == 0) ? true : false;
+    String x = (isEven) ? "even" : "odd";
+    System.out.println("Your number is " + x);
     //once finished
     reader.close();
   }

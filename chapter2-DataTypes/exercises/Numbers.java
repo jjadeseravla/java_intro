cf16f0f0933bf7d875e62d3c0594634fb7c7c0cd
jjade seravla <jadealvares@hotmail.co.uk>
2017-12-20 10:28:41 +0000

    completed numbers in chap2

--- chapter2-DataTypes/exercises/Numbers.java
+++ chapter2-DataTypes/exercises/Numbers.java
@@ -1,12 +1,12 @@
 public class Numbers {
   public static void main(String[] args) {
-    // declare an integer variable with a value
+      // declare an integer variable with a value
+  int first_number = 10;
+      // declare a double variable with a value
+  double second_number = 9;
+      // store the result of adding the two together in a variable
 
-    // declare a double variable with a value
-
-    // store the result of adding the two together in a variable
-
-    // print it
-
+      // print it
+  System.out.println(first_number + second_number);
   }
-}
+ }

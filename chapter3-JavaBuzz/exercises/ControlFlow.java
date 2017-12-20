f89d1f8528f054ad90516f7354215cc96afae703
jjade seravla <jadealvares@hotmail.co.uk>
2017-12-20 11:50:18 +0000

    enter a number in exercise 3

--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -8,3 +8,22 @@
 // (assume user only ever enters integers)
 
 // print the answer to the console
+import java.util.Scanner;
+//
+// public class ControlFlow {
+//   public static void main(String[] args) {
+//     Scanner scan = new Scanner(System.in);
+//     // String s = scan.next();
+//     // int i = scan.nextInt();
+//   }
+// }
+
+ public class ControlFlow {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);  // Reading from System.in
+    System.out.println("Enter a number: ");
+    int n = reader.nextInt(); // Scans the next token of the input as an int.
+    //once finished
+    reader.close();
+  }
+}
